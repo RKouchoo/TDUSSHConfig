206e4f448a325782534cca04a0e096fe9adaf6a3
RKouchoo <rkouchoo1@gmail.com>
2018-03-07 17:12:37 +1100

    commenting, not worthy to go in build

--- SSHRobotConfig/src/org/team3132/rkouchoo/main/Constants.java
+++ SSHRobotConfig/src/org/team3132/rkouchoo/main/Constants.java
@@ -2,6 +2,10 @@ package org.team3132.rkouchoo.main;
 
 public class Constants {
 
+	/**
+	 * All the global static variables referenced by the robot.
+	 */
+	
 	public static final String SSH_USER = "lvuser";
 	public static final String SSH_PASSWORD = "";
 	public static final String HOST_ADDRESS = "roborio-3132-frc.local";
